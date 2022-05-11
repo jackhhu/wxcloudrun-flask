ca516caed4412557345dc9b7776a3b89c8f1bc48
jackhhu <49669049+jackhhu@users.noreply.github.com>
2022-05-11 12:15:23 +0800

    Update views.py

--- wxcloudrun/views.py
+++ wxcloudrun/views.py
@@ -52,7 +52,7 @@ def index():
         options = webdriver.ChromeOptions()
         options.add_argument('--no-sandbox')
         options.add_argument('--disable-extensions')
-        options.add_argument('--headless')
+#         options.add_argument('--headless')
         options.add_argument('--disable-gpu')
 
         driver = webdriver.Chrome(chrome_options=options)    
