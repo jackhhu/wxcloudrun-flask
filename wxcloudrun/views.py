9384c604e5d2561c9282f2fca4750233e9780890
jackhhu <49669049+jackhhu@users.noreply.github.com>
2022-05-11 19:48:15 +0800

    Update views.py

--- wxcloudrun/views.py
+++ wxcloudrun/views.py
@@ -35,7 +35,7 @@ import akshare as ak
 
 @app.route("/")
 def index():
-   futuredata=[]
+    futuredata=[]
     missions = ['ZC2209','RB2210','HC2210','J2209','JM2209','I2209','SF2209','SM2209',
                 # 'FG2209','SA2209','AU2206','AG2206','CU2206','AL2206','ZN2206','PB2206',
                 ]
@@ -107,7 +107,6 @@ def index():
         i = i+1
     
     print (futuredata)
-
 
     return json.dumps(futuredata)
 
